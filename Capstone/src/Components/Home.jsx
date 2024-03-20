a9e49bdf5abf5eb3fc2decdc9e298f3ef59e07d1
Brandon Babineau <babineau1991@gmail.com>
2024-03-20 19:55:05 -0400

    added animation to homepage

--- Capstone/src/Components/Home.jsx
+++ Capstone/src/Components/Home.jsx
@@ -6,6 +6,7 @@ function Home() {
     <div>
       <h1>AvenueMart</h1>
       <img className="main" src="https://images.pexels.com/photos/1707213/pexels-photo-1707213.jpeg" />
+      <div className="pulser"></div>
     </div>
   );
 }
